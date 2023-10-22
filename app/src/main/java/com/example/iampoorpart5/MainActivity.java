6b5d31367d31d1856a9c082c27558b540d8b98ac
rhrie <xkklarke@gmail.com>
2023-10-21 22:49:58 -0700

    first commit

--- app/src/main/java/com/example/iampoorpart5/MainActivity.java
+++ app/src/main/java/com/example/iampoorpart5/MainActivity.java
@@ -1,3 +1,8 @@
+/*
+Name: Rachael
+Date: 10/19/2023
+Project Name: I am poor part 5
+ */
 package com.example.iampoorpart5;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,36 +23,36 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        button = (Button)findViewById(R.id.button);
-        button2 = (Button)findViewById(R.id.button2);
-        text = findViewById(R.id.textView);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button.setOnClickListener(new View.OnClickListener()
-        {
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        text = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (button.getText().toString().equals("RICH")) {
-                    text.setText("I AM RICH");
-                    text.setTextColor(Color.RED);
-                }
-                else{
-                    text.setText("I AM RICH");
-                }
+                verifyRichness(button);
             }
+
         });
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (button2.getText().toString().equals("POOR")) {
-                    text.setText("I AM POOR");
-                    text.setTextColor(Color.GREEN);
-                } else {
-                    text.setText("I AM RICH");
-                }
+                verifyRichness(button2);
             }
         });
+    }
+    private void verifyRichness(Button button){
+        String buttonText = button.getText().toString();
+
+        if (buttonText.equals("RICH")) {
+            text.setText("I AM RICH");
+        }
+        else if (buttonText.equals("POOR")) {
+            text.setText("I AM POOR");
+        }
     }
 }
